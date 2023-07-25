31a4b705ea23caa908e1b6c7888bee19adbbfb25
Muhammad Hamza <113111932+muhammadhamza895@users.noreply.github.com>
2023-07-25 06:31:52 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('E:/IEC copy/images/background.png')",
+        'last': "url('E:/IEC copy/images/last.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
